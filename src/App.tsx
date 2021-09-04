a2b76c25cd0bc2715c326e03db6628235fb98edf
신유나 <yunadev01@gmail.com>
2021-09-05 02:13:20 +0900

    Setting folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Redirect path="/" to="/home" exact />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
