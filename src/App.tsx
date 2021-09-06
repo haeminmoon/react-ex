f2cf03f304935f2e412318d2a06eafb3c6ed5d6b
ShinYuna <yn.shin@bfactory.inc>
2021-09-06 15:39:35 +0900

    [Update] App routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,22 @@ import AppLayout from './components/AppLayout';
 
 const Home = loadable(() => import('./pages/Home'));
 const Live = loadable(() => import('./pages/Live'));
+const Rank = loadable(() => import('./pages/Rank'));
+const My = loadable(() => import('./pages/My'));
 
 function App() {
   return (
     <AppLayout>
       <AppLayout.Main>
         <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/live" component={Live} />
-          <Redirect from="/" to="/home" />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/live" component={Live} />
+          <Route exact path="/rank" component={Rank} />
+          <Route exact path="/my" component={My} />
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Redirect exact path="*" to="/" />
         </Switch>
       </AppLayout.Main>
     </AppLayout>
