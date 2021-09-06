112f52e157dd2fca668c27135e494115fa57cda5
ShinYuna <yn.shin@bfactory.inc>
2021-09-06 16:35:04 +0900

    [Create] Image Carousel - install plugin, @egjs/flicking-plugins

---
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,32 @@
+import Flicking, { ViewportSlot } from '@egjs/react-flicking';
+import { Pagination, AutoPlay } from '@egjs/flicking-plugins';
+
+import '@egjs/react-flicking/dist/flicking.css';
+import '@egjs/flicking-plugins/dist/pagination.css';
+import './index.scss';
+
+type ImageCarouselProps = {
+  resources?: string[];
+  children?: React.ReactChild;
+};
+
+function ImageCarousel({ resources, children }: ImageCarouselProps) {
+  const _plugins = [
+    new Pagination({ type: 'bullet' }),
+    new AutoPlay({ duration: 3000, direction: 'NEXT', stopOnHover: true }),
+  ];
+
+  return (
+    <Flicking circular={true} plugins={_plugins}>
+      {children}
+      <div className="w-full h-52 bg-red-200">1</div>
+      <div className="w-full h-52 bg-blue-200">2</div>
+      <div className="w-full h-52 bg-green-200">3</div>
+      <ViewportSlot>
+        <div className="flicking-pagination"></div>
+      </ViewportSlot>
+    </Flicking>
+  );
+}
+
+export default ImageCarousel;
