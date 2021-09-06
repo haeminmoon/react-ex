b36c47b29fb9a90f3443325b966879db943305ba
ShinYuna <yn.shin@bfactory.inc>
2021-09-07 02:53:40 +0900

    [Update] Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
+import { eventBanners } from '~/constants/mock';
+
 import Categries from '~/components/Categires/Categries';
 import ImageCarousel from '~/components/ImageCarousel/ImageCarousel';
 import Feed from '~/components/Feed';
-
-import { eventBanners } from '~/constants/mock';
+import FilterModal from '~/components/FilterModal';
 
 function Home() {
+  const [filterVisible, setFilterVisible] = useState<boolean>(false);
+
   return (
     <>
       <ImageCarousel resources={eventBanners} />
-      <Categries />
+      <Categries onFilterModal={() => setFilterVisible(true)} />
       <Feed />
+      <FilterModal visible={filterVisible} onClose={() => setFilterVisible(false)} />
     </>
   );
 }
