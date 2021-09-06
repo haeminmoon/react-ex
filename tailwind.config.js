4562558bc06e3c6b4bebe519f38c375ae63104c8
ShinYuna <yn.shin@bfactory.inc>
2021-09-06 15:38:54 +0900

    [Update] extends tailwind boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        top: '0px 10px 0px rgba(0, 0, 0, 0.05), 0px -5px 10px rgba(0, 0, 0, 0.05)',
+      },
+    },
   },
   variants: {
     extend: {},
