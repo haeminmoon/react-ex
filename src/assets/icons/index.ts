881e79b08dd3e9d70d829f4111da8148ad540c0c
ShinYuna <yn.shin@bfactory.inc>
2021-09-06 15:41:10 +0900

    [Update] assets - logo2 image - material ui icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CalendarIcon } from '@material-ui/icons/EventNote';
+export { default as RankIcon } from '@material-ui/icons/EmojiEvents';
+export { default as UserIcon } from '@material-ui/icons/PermIdentity';
+export { default as SearchIcon } from '@material-ui/icons/Search';
+export { default as NotificationIcon } from '@material-ui/icons/NotificationsNone';
