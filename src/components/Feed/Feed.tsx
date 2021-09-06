fb4a3843ba03f19bba7330179a623a55f4918b70
ShinYuna <yn.shin@bfactory.inc>
2021-09-07 02:53:56 +0900

    [Update] Feed items key

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,11 +1,11 @@
-import { productList } from '~/constants/mock';
 import LiveItem from '../LiveItem';
+import { productList } from '~/constants/mock';
 
 function Feed() {
   return (
     <section className="p-6 grid gap-6">
       {productList.map(product => (
-        <LiveItem product={product} />
+        <LiveItem product={product} key={product.id} />
       ))}
     </section>
   );
