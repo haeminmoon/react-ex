4f51a87ea7c1f2e3563bb35dbde98beecaef12cd
신유나 <yunadev01@gmail.com>
2021-09-12 18:05:30 +0900

    [Update] live - live 컴포넌트 props 타입 지정 - 스타일 수정

--- src/pages/Live/Live.tsx
+++ src/pages/Live/Live.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+import { liveData } from '~/constants/mock';
+
 import LiveItem from '~/components/Live';
 
 function Live() {
+  const [palyItem, setPlayItem] = useState<number>(2);
+  const playLive = liveData[palyItem];
+
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50">
-      <LiveItem />
+      <LiveItem
+        loading={false}
+        title={playLive.title}
+        videoImage={playLive.videoImage}
+        videoSource={playLive.videoSource}
+        mallName={playLive.mallName}
+        mallLink={playLive.mallLink}
+      />
     </div>
   );
 }
