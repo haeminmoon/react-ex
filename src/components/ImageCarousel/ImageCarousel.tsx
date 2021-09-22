78946a956737a93f22415b03901b2ce227299fae
신유나 <yunadev01@gmail.com>
2021-09-23 01:09:13 +0900

    update: 메인, 이미지 캐로셀

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -22,7 +22,7 @@ function ImageCarousel({ resources }: ImageCarouselProps) {
   ];
 
   return (
-    <Flicking circular={true} plugins={_plugins} autoResize={true}>
+    <Flicking plugins={_plugins} circular autoResize preventClickOnDrag moveType="snap" inputType={['touch']}>
       {resources?.map((resource, index) => (
         <div className="w-full bg-gray-100 h-52" key={`${resource.event}-${index}`}>
           <a href={resource.link} key={resource.link} className="block w-full h-full">
